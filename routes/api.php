02ceb7a9d7cf6c00d8761f65c97bf5bbd354d7fb
HamzaMalik-1 <hamzamalik2048@gmail.com>
2025-09-07 02:07:29 +0500

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
@@ -6,21 +7,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\InterviewController;
 
+// Get authenticated user
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Authentication
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::post('/login',[AuthController::class,'login'])->name('login');
+// Admin
+Route::get('/admin', [UserController::class, 'getAdmin'])->name('admin.get');
 
-Route::get('/admin', [UserController::class,'getAdmin'])->name('getAdmin');
+// Categories
+Route::get('/categories', [CategoryController::class, 'getCategories'])->name('categories.index');
 
-Route::get('/categories',[CategoryController::class,'getCategories'])->name('categories');
-
-Route::get('/interview',[InterviewController::class,'getAllOrPagination'])->name('interview');
-
-Route::post('/add-interview',[InterviewController::class,'addInterview'])->name('interview');
-
-Route::get('/delete-interview/{id}',[InterviewController::class,'deleteInterview'])->name('deleteInterview');
-
-Route::post('/edit-interview',[InterviewController::class,'editInterview'])->name('editInterview');
+// Interviews
+Route::get('/interview', [InterviewController::class, 'getAllOrPagination'])->name('interview.index');
+Route::post('/add-interview', [InterviewController::class, 'addInterview'])->name('interview.store');
+Route::get('/delete-interview/{id}', [InterviewController::class, 'deleteInterview'])->name('interview.delete');
+Route::post('/edit-interview', [InterviewController::class, 'editInterview'])->name('interview.update');
